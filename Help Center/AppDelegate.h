0650230798b811c4cd05426df759b910a4f04c34
beccaisageek <rebeccaslinkard@nbp-186-73.nbp.ruw.rutgers.edu>
2014-10-11 16:01:32 -0400

    Initial Commit

---
+++ Help Center/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Help Center
+//
+//  Created by Becca on 10/11/14.
+//  Copyright (c) 2014 KBS Programming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Help Center/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Help Center
+//
+//  Created by Becca on 10/11/14.
+//  Copyright (c) 2014 KBS Programming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
